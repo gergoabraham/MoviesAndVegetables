32538258d491d23958ccf06a8a47763e2d75d217
gergooo <39014407+gergooo@users.noreply.github.com>
2020-07-21 14:23:43 +0200

    fetch() fake response contains url (+redirection)

--- test/unit/fakes/fetchFake-test.js
+++ test/unit/fakes/fetchFake-test.js
@@ -24,31 +24,47 @@ describe('fetch fake', function() {
     });
   });
 
-  context('fetch', function() {
-    it('should return with response object incl. text()', async function() {
+  context('fetch(): response object', function() {
+    it('should contain text()', async function() {
       const returnValue = await fetch('https://fetch-fake.com/test-file-to-load');
 
-      returnValue.should.have.key('text');
+      returnValue.should.contain.key('text');
       (typeof returnValue.text).should.equal('function');
+    });
+
+    it('should contain url', async function() {
+      const returnValue = await fetch('https://fetch-fake.com/test-file-to-load');
+
+      returnValue.should.contain.key('url');
+      returnValue.url.should.equal('https://fetch-fake.com/test-file-to-load');
     });
   });
 
-  context('text()', function() {
-    it('should fetch x.com/y', async function() {
+  context('file handling', function() {
+    it('fetch() should throw error if file is not found', async function() {
+      await fetch('www.no-such-file.com').should.be
+          .rejectedWith('fetch() fake: no file matches the url.\n\n' +
+                        'url: www.no-such-file.com\n\n' +
+                        'filename: no-such-file.html');
+    });
+
+    it('text() should return file content', async function() {
       const response = await fetch('fetch-fake.com/test-file-to-load');
       const text = await response.text();
 
       text.should.equal('You found the first one!');
     });
+  });
 
-    it('should handle "www."', async function() {
+  context('input url handling', function() {
+    it('should handle missing "https://"', async function() {
       const response = await fetch('www.fetch-fake.com/test-file-to-load');
       const text = await response.text();
 
       text.should.equal('You found the first one!');
     });
 
-    it('should handle https://', async function() {
+    it('should handle missing "www."', async function() {
       const response = await fetch('https://fetch-fake.com/test-file-to-load');
       const text = await response.text();
 
@@ -56,17 +72,27 @@ describe('fetch fake', function() {
     });
 
     it('should fetch subpage (x.com/y/z)', async function() {
-      const response = await fetch('https://www.fetch-fake.com/test-file-to-load/another-one');
+      const response = await fetch('fetch-fake.com/test-file-to-load/another-one');
       const text = await response.text();
 
       text.should.equal('You found the second one!');
     });
 
-    it('should throw error to fail test if file is not found', async function() {
-      await fetch('www.no-such-file.com').should.be
-          .rejectedWith('fetch() fake: no file matches the url.\n\n' +
-           'url: www.no-such-file.com\n\n' +
-           'filename: no-such-file.html');
+    it('should handle query parameters (x.com/y/z?a=2&b=3)', async function() {
+      const response = await fetch(
+          'https://www.fetch-fake.com/test-file-to-load/another-one?a=2&b=3');
+      const text = await response.text();
+
+      text.should.equal('Wow, parameters!');
+    });
+  });
+
+  context('url redirection', function() {
+    it('url should be changed if fetched file contains "---> [new url]"', async function() {
+      const returnValue = await fetch('https://fetch-fake.com/redirected-page');
+
+      returnValue.should.contain.key('url');
+      returnValue.url.should.equal('https://redirected.com/here-we-are');
     });
   });
 });
--- test/unit/fakes/fetchFake.js
+++ test/unit/fakes/fetchFake.js
@@ -5,20 +5,25 @@
  */
 'use strict';
 
-function activateFetchFake(url) {
+function activateFetchFake() {
   global.fetch = fetchFake;
 }
 
 async function fetchFake(url) {
-  const fileName = `${convertToFileName(url)}.html`;
+  const fileName = convertToFileName(url);
   const filePath = `./test/unit/html/${fileName}`;
 
   if (fs.existsSync(filePath)) {
-    return {text: async () => fs.readFileSync(filePath).toString()};
+    const fileContent = fs.readFileSync(filePath).toString();
+
+    return {
+      url: getDocumentUrl(fileContent, url),
+      text: async () => fileContent,
+    };
   } else {
     throw new Error(`fetch() fake: no file matches the url.\n\n`+
-    `url: ${url}\n\n` +
-    `filename: ${fileName}`);
+                    `url: ${url}\n\n` +
+                    `filename: ${fileName}`);
   }
 }
 
@@ -26,7 +31,20 @@ function convertToFileName(url) {
   return url.replace(/^https:\/\//, '')
       .replace(/^www\./, '')
       .replace(/\.com/, '')
-      .replace(/\//g, '.');
+      .replace(/\//g, '.')
+      .replace(/\?/, '...') +
+
+      '.html';
+}
+
+function getDocumentUrl(fileContent, originalUrl) {
+  const redirectedUrl = fileContent.match(/^---> (.+)$/);
+
+  if (redirectedUrl) {
+    return redirectedUrl[1];
+  } else {
+    return originalUrl;
+  }
 }
 
 module.exports = {activateFetchFake};
