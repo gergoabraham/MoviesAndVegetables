8cc5b7be519c94f54e021912479920a241c2abdf
gergooo <39014407+gergooo@users.noreply.github.com>
2020-07-24 10:04:14 +0200

    end-to-end test improvement

--- test/end-to-end/endToEndTests.js
+++ test/end-to-end/endToEndTests.js
@@ -23,6 +23,50 @@ describe('End-to-end tests', async function() {
     await installAddon();
   });
 
+  context('on IMDb', function() {
+    before(async function() {
+      await driver.get('https://www.imdb.com/title/tt6751668/');
+    });
+
+    it('should inject audience score', async function() {
+      await driver.wait(until.elementLocated(By.id('audience-score')), 10000);
+      const audienceScore = (await driver).findElement(By.id('audience-score'));
+
+      audienceScore.should.exist;
+    });
+
+    it('should inject Tomatometer', async function() {
+      await driver.wait(
+          until.elementLocated(By.className('titleReviewBarItem TomatoMeter')),
+          1000);
+
+      const tomatoMeter = await driver
+          .findElement(By.className('titleReviewBarItem TomatoMeter'));
+
+      tomatoMeter.should.exist;
+    });
+  });
+
+  context('on RottenTomatoes', function() {
+    before(async function() {
+      await driver.get('https://www.rottentomatoes.com/m/the_dark_knight');
+    });
+
+    it('should inject IMDb scores', async function() {
+      await driver.wait(until.elementLocated(By.id('IMDb scores')), 10000);
+      const imdbScores = await driver.findElement(By.id('IMDb scores'));
+
+      imdbScores.should.exist;
+    });
+  });
+
+  after(async function() {
+    driver.quit();
+  });
+
+
+  /* --- Helpers --- */
+
   function rebuildAddon() {
     cleanupArtifacts();
     return new Promise((resolve) => cmd.get('npm run build', resolve));
@@ -55,47 +99,5 @@ describe('End-to-end tests', async function() {
 
     return driver.installAddon(`${addonFolder}/${addonFileName}`, true);
   }
-
-  context('on IMDb', function() {
-    before(async function() {
-      await driver.get('https://www.imdb.com/title/tt6751668/');
-    });
-
-    it('should inject audience score', async function() {
-      await driver.wait(until.elementLocated(By.id('audience-score')), 10000);
-      const audienceScore = (await driver).findElement(By.id('audience-score'));
-
-      audienceScore.should.exist;
-    });
-
-    it('should inject Tomatometer', async function() {
-      await driver.wait(
-          until.elementLocated(By.className('titleReviewBarItem TomatoMeter')),
-          1000);
-
-      const tomatoMeter = await driver
-          .findElement(By.className('titleReviewBarItem TomatoMeter'));
-
-      tomatoMeter.should.exist;
-    });
-  });
-
-  context('on RottenTomatoes', function() {
-    before(async function() {
-      driver.get('https://www.rottentomatoes.com/m/the_dark_knight')
-          .catch((err) => {}); // it is loaded more than the 5 seconds timeout
-    });
-
-    it('should inject IMDb scores', async function() {
-      await driver.wait(until.elementLocated(By.id('IMDb scores')), 10000);
-      const imdbScores = await driver.findElement(By.id('IMDb scores'));
-
-      imdbScores.should.exist;
-    });
-  });
-
-  after(async function() {
-    driver.quit();
-  });
 });
 
