b07a63074231e8a445f54169b4cb9a2c4d41626d
gergooo <39014407+gergooo@users.noreply.github.com>
2021-05-24 16:38:04 +0200

    improved tomatometer logo reading

--- src/MoviePages/RottenPage.js
+++ src/MoviePages/RottenPage.js
@@ -115,7 +115,7 @@ class RottenPage extends MoviePage {
   }
 
   _readTomatometerFreshness() {
-    return this._text.match(/"tomatometerState":"(\w+)"/)[1];
+    return this._text.match(/"tomatometerState":"([\w-]+)"/)[1];
   }
 
   async _readUserRatings() {
@@ -153,7 +153,7 @@ class RottenPage extends MoviePage {
   }
 
   _readAudienceScoreFreshness() {
-    return this._text.match(/"audienceState":"(\w+)"/)[1];
+    return this._text.match(/"audienceState":"([\w-]+)"/)[1];
   }
 
   async _readLogoUrl(freshness) {
